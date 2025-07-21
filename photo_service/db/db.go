699ea08a25f8b00d625aebe6c902db15a66ebdd9
Sue1996-bits <yifanh09@gmail.com>
2025-07-21 15:08:04 +0800

    Update db.go

--- photo_service/db/db.go
+++ photo_service/db/db.go
@@ -1,53 +1,25 @@
-package db
-
 import (
-	"time"
+	"log"
+	"photo_service/models"
 
 	"gorm.io/driver/sqlite"
-	"gorm.io/gorm/logger"
 
 	"gorm.io/gorm"
 )
 
-//2.0数据库初始化：连接、配置、迁移、索引
+var DB *gorm.DB
 
-// 重写config（GORM）
-type Config struct {
-	DatabasePath    string
-	MaxOpenConns    int
-	MaxIdleConns    int
-	ConnMaxLifetime time.Duration
-	LogLevel        logger.LogLevel //日志等级
-}
-
-func InitDatabase(config *Config) (*gorm.DB, error) {
+func InitDatabase() {
+	var err error
 	// gorm.Open 函数来打开一个 SQLite 数据库文件,并传入一个 gorm.Config 结构体来配置一些选项。
-	db, err := gorm.Open(sqlite.Open(config.DatabasePath), &gorm.Config{
-		Logger: logger.Default.LogMode(config.LogLevel),
-	}) //!!!注意这里是:= /  = 即赋值而非新建！！！
+	DB, err = gorm.Open(sqlite.Open("photo.db"), &gorm.Config{}) //!!!注意这里不是:= 是= 即赋值而非新建！！！
 	if err != nil {
-		return nil, err
+		log.Fatal("数据库连接失败:", err)
 	}
-
-	sqlDB, err := db.DB()
-	if err != nil {
-		return nil, err
+	// 自动建表:db.AutoMigrate 方法来根据结构体定义自动创建数据表。
+	if err := DB.AutoMigrate(&models.EncryptedImage{}); err != nil {
+		log.Fatal("自动迁移失败:", err)
 	}
-
-	// 连接池配置
-	sqlDB.SetMaxOpenConns(config.MaxOpenConns)
-	sqlDB.SetMaxIdleConns(config.MaxIdleConns)
-	sqlDB.SetConnMaxLifetime(config.ConnMaxLifetime)
-
-	// 执行迁移
-	if err := MigrateWatermarkLog(db); err != nil {
-		return nil, err
-	}
-
-	if err := CreateIndexes(db); err != nil {
-		return nil, err
-	}
-
-	return db, nil
+	log.Println("✅ 数据库初始化完成")
 
 }
